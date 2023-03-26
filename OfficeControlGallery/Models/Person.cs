a72a1ff219f2b545417bee2b41eff9da9905f65f
aren <aren@what.codes>
2023-03-26 22:39:11 +0800

    add collection view

---
+++ OfficeControlGallery/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace OfficeControlGallery.Models;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Location { get; set; }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
---
+++ OfficeControlGallery/Pages/DataTemplatePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace OfficeControlGallery.Pages;
+
+public partial class DataTemplatePage : ContentPage
+{
+	public DataTemplatePage()
+	{
+		InitializeComponent();
+	}
+}
