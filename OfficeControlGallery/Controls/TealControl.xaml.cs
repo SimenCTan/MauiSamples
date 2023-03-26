027f18679712c9432a2afa3da28ac96662e2ff16
aren <aren@what.codes>
2023-03-26 20:38:35 +0800

    add data template

--- OfficeControlGallery/Controls/TealControl.xaml.cs
+++ OfficeControlGallery/Controls/TealControl.xaml.cs
@@ -2,14 +2,6 @@ namespace OfficeControlGallery.Controls;
 
 public partial class TealControl : ContentView
 {
-    Label themeLabel;
-    public static readonly BindableProperty HeaderTextProperty = BindableProperty.Create(nameof(HeaderText), typeof(string), typeof(TealControl), string.Empty);
-
-    public string HeaderText
-    {
-        get => (string)GetValue(HeaderTextProperty);
-        set => SetValue(HeaderTextProperty, value);
-    }
     public TealControl()
     {
         InitializeComponent();
@@ -18,12 +10,5 @@ public partial class TealControl : ContentView
     private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
 
-    }
-
-    protected override void OnApplyTemplate()
-    {
-        base.OnApplyTemplate();
-        themeLabel = (Label)GetTemplateChild("changeThemeLabel");
-        themeLabel.Text ="Aqua Theme";
     }
 }
--- OfficeControlGallery/Pages/ContentPresenterPage.xaml.cs
+++ OfficeControlGallery/Pages/ContentPresenterPage.xaml.cs
@@ -2,8 +2,24 @@ namespace OfficeControlGallery.Pages;
 
 public partial class ContentPresenterPage : ContentPage
 {
-	public ContentPresenterPage()
+    public static readonly BindableProperty HeaderTextProperty = BindableProperty.Create(nameof(HeaderText), typeof(string), typeof(ContentPresenterPage), string.Empty);
+
+    public string HeaderText
+    {
+        get => (string)GetValue(HeaderTextProperty);
+        set => SetValue(HeaderTextProperty, value);
+    }
+    public ContentPresenterPage()
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        var headerLabel =(Label)GetTemplateChild("headerLabel");
+        headerLabel.Text = "MyApp";
+        var themeLabel = (Label)GetTemplateChild("changeThemeLabel");
+        themeLabel.Text = "Aqua Theme";
+    }
 }
