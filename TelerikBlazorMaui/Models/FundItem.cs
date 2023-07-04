3ffab77d944fa6dea6f1d14301a959d331328949
aren <aren@what.codes>
2023-07-04 19:38:45 +0800

    add telerik tree view

---
+++ TelerikBlazorMaui/Models/FundItem.cs
@@ -0,0 +1,13 @@
+﻿using Telerik.SvgIcons;
+
+namespace TelerikBlazorMaui.Models;
+
+public class FundItem
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public int? ParentIdValue { get; set; }
+    public bool HasChildren { get; set; }
+    public ISvgIcon Icon { get; set; }
+    public bool Expanded { get; set; }
+}
