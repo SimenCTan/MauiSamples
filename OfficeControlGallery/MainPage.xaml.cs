24dee14cec7a53ae20b2bcabcf58ac7aed1257d4
aren <aren@what.codes>
2023-08-18 23:56:21 +0800

    add chart view

--- OfficeControlGallery/MainPage.xaml.cs
+++ OfficeControlGallery/MainPage.xaml.cs
@@ -1,24 +1,44 @@
-﻿namespace OfficeControlGallery
+﻿using Microcharts;
+
+using SkiaSharp;
+
+namespace OfficeControlGallery
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        ChartEntry[] entries = new[]
+        {
+            new ChartEntry(112)
+            { 
+                Label="Windows",
+                ValueLabel="112",
+                Color=SKColor.Parse("#129032") 
+            },
+            new ChartEntry(212)
+            {
+                Label="Android",
+                ValueLabel="212",
+                Color=SKColor.Parse("#123109")
+            },
+            new ChartEntry(100)
+            {
+                Label="IOS",
+                ValueLabel="100",
+                Color=SKColor.Parse("#121032")
+            },
+            new ChartEntry(80)
+            {
+                Label="Mac",
+                ValueLabel="80",
+                Color=SKColor.Parse("#123909")
+            },
+        };
 
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            chartView1.Chart = new BarChart { Entries = entries };
+            chartView2.Chart= new LineChart { Entries = entries };
         }
     }
 }
